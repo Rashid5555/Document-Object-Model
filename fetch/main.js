f784b192b48f4ef7f0a674b5667e7f1c7629691c
Rashid404 <shalbuzovrashid@gmail.com>
2022-06-22 16:27:02 +0400

    release v1

---
+++ fetch/main.js
@@ -0,0 +1,29 @@
+// function myRandom() {
+//     let myRandom = Math.floor(Math.random() * 10)
+//     let count = 0
+//     while (true) {
+//         myRandom = Math.floor(Math.random() * 100)
+//         if (myRandom == 5) {
+//             console.log(count + "-ci cehdinde " + myRandom + " tapdiniz");
+//             break
+//         }
+//         count++
+//     }
+// }
+
+// myRandom()
+
+
+
+// let myArr = [1, 4, 8, 6, 99];
+
+// let maxNumber = myArr[0];
+// for (let = i)
+
+
+
+// let myArr = [1,14,28,16,109,-10]
+
+// console.log(myArr.sort(function(x,y){
+//     return x-y
+// }));
